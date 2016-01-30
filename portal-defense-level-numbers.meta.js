2efd110284fbe352683f5265f926cc970a4527fb
IlGuru <s.busnelli@tin.it>
2016-01-30 19:10:44 +0100

    000009

---
+++ portal-defense-level-numbers.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-portal-defense-level@ilguru
+// @name           IITC plugin: Portal Defense Level
+// @category       Layer
+// @version        1.0.9.20160130.000009
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/IlGuru/IITC/blob/master/portal-defense-level-numbers.meta.js
+// @downloadURL    https://github.com/IlGuru/IITC/blob/master/portal-defense-level-numbers.user.js
+// @description    [ilguru-2016-01-30-000009] Show portal defense level on map.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
--- portal-defense-level-numbers.user.js
+++ portal-defense-level-numbers.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-portal-defense-level@ilguru
 // @name           IITC plugin: Portal Defense Level
 // @category       Layer
-// @version        1.0.8.20160130.000008
+// @version        1.0.9.20160130.000009
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-//// @updateURL      https://secure.jonatkins.com/iitc/release/plugins/portal-defense-level-numbers.meta.js
-//// @downloadURL    https://secure.jonatkins.com/iitc/release/plugins/portal-defense-level-numbers.user.js
-// @description    [ilguru-2016-01-30-000008] Show portal defense level on map.
+// @updateURL      https://github.com/IlGuru/IITC/blob/master/portal-defense-level-numbers.meta.js
+// @downloadURL    https://github.com/IlGuru/IITC/blob/master/portal-defense-level-numbers.user.js
+// @description    [ilguru-2016-01-30-000009] Show portal defense level on map.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
